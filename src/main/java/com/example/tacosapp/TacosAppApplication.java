f74539bda77c2763bf8123c3f0a6d3a67ee08945
Soluckyo <twinki0000@yandex.ru>
2024-05-06 16:59:18 +0300

    First commit

---
+++ src/main/java/com/example/tacosapp/TacosAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.tacosapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TacosAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TacosAppApplication.class, args);
+	}
+
+}
