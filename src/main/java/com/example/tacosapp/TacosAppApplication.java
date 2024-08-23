d1b85512a66e40c0c9e4355788bd0034d71ca436
Soluckyo <twinki0000@yandex.ru>
2024-08-23 20:46:07 +0300

    added the tacoById, postTaco, and putTaco methods to the Api

--- src/main/java/com/example/tacosapp/TacosAppApplication.java
+++ src/main/java/com/example/tacosapp/TacosAppApplication.java
@@ -3,12 +3,10 @@ package com.example.tacosapp;
 import com.example.tacosapp.data.IngredientRepository;
 import com.example.tacosapp.Ingredient.Type;
 import com.example.tacosapp.data.TacoRepository;
-import com.example.tacosapp.data.UserRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
--- src/main/java/com/example/tacosapp/api/TacoApi.java
+++ src/main/java/com/example/tacosapp/api/TacoApi.java
@@ -1,24 +1,55 @@
 package com.example.tacosapp.api;
 
 import com.example.tacosapp.Taco;
+import com.example.tacosapp.TacoOrder;
+import com.example.tacosapp.data.OrderRepository;
 import com.example.tacosapp.data.TacoRepository;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping(path = "/api/tacos", produces = "application/json")
 @CrossOrigin(origins = {"http:/tacoapp:8080", "http://tacoapp.com"})
 public class TacoApi {
     private final TacoService tacoService;
+    private final TacoRepository tacoRepository;
+    private final OrderRepository orderRepository;
 
-    public TacoApi(TacoService tacoService) {
+    public TacoApi(TacoService tacoService, TacoRepository tacoRepository, OrderRepository orderRepository) {
         this.tacoService = tacoService;
+        this.tacoRepository = tacoRepository;
+        this.orderRepository = orderRepository;
     }
 
     @GetMapping(params = "recent")
-    public List<Taco> recent(@RequestParam int page, @RequestParam int size) {
+    public List<Taco> recent(@RequestParam(defaultValue = "0") int page, @RequestParam(defaultValue = "10") int size) {
         return tacoService.findAll(PageRequest.of(page, size));
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Taco> tacoById(@PathVariable Long id) {
+        Optional<Taco> optTaco = tacoRepository.findById(id);
+        if (optTaco.isPresent()) {
+            return new ResponseEntity<>(optTaco.get(), HttpStatus.OK);
+        }
+        return new ResponseEntity<>(null, HttpStatus.NOT_FOUND);
+    }
+
+    @PostMapping(consumes = "application/json")
+    @ResponseStatus(HttpStatus.CREATED)
+    public Taco postTaco(@RequestBody Taco taco){
+        return tacoRepository.save(taco);
+    }
+
+    @PutMapping(path = "/{orderId}", consumes = "application/json")
+    public TacoOrder putOrder(@PathVariable Long orderId, @RequestBody TacoOrder order){
+        order.setId(orderId);
+        return orderRepository.save(order);
+
     }
 }
