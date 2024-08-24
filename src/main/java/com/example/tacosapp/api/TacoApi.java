3e6714ce7e4dac8894f13fec38a31458548aecba
Soluckyo <twinki0000@yandex.ru>
2024-08-24 19:04:34 +0300

    added the delete and patch methods to the Api

--- src/main/java/com/example/tacosapp/api/TacoApi.java
+++ src/main/java/com/example/tacosapp/api/TacoApi.java
@@ -4,6 +4,7 @@ import com.example.tacosapp.Taco;
 import com.example.tacosapp.TacoOrder;
 import com.example.tacosapp.data.OrderRepository;
 import com.example.tacosapp.data.TacoRepository;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -51,5 +52,43 @@ public class TacoApi {
         order.setId(orderId);
         return orderRepository.save(order);
 
+    }
+
+    @PatchMapping(path = "/{orderId}", consumes = "application/json")
+    public TacoOrder patchOrder(@PathVariable Long orderId, @RequestBody TacoOrder patch){
+        TacoOrder order = orderRepository.findById(orderId).get();
+        if(patch.getDeliveryName() != null){
+            order.setDeliveryName(patch.getDeliveryName());
+        }
+        if(patch.getDeliveryStreet() != null){
+            order.setDeliveryStreet(patch.getDeliveryStreet());
+        }
+        if(patch.getDeliveryCity() != null){
+            order.setDeliveryCity(patch.getDeliveryCity());
+        }
+        if(patch.getDeliveryState() != null){
+            order.setDeliveryState(patch.getDeliveryState());
+        }
+        if(patch.getDeliveryZip() != null){
+            order.setDeliveryZip(patch.getDeliveryZip());
+        }
+        if(patch.getCcNumber() != null){
+            order.setCcNumber(patch.getCcNumber());
+        }
+        if (patch.getCcExpiration() != null){
+            order.setCcExpiration(patch.getCcExpiration());
+        }
+        if (patch.getCcCVV() != null){
+            order.setCcCVV(patch.getCcCVV());
+        }
+        return orderRepository.save(order);
+    }
+
+    @DeleteMapping("/{orderId}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deleteOrder(@PathVariable Long orderId){
+        try{
+            orderRepository.deleteById(orderId);
+        }catch (EmptyResultDataAccessException e){}
     }
 }
